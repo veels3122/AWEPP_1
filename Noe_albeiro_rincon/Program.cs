855d8904a3b3a6a19c7ffa56f4643513c0a76b0a
veels3122 <mmelgarejo@ucundinamarca.edu.co>
2024-09-03 19:27:48 -0500

    agregacion de noe

---
+++ Noe_albeiro_rincon/Program.cs
@@ -0,0 +1,30 @@
+using Microsoft.EntityFrameworkCore;
+
+var builder = WebApplication.CreateBuilder(args);
+
+//add services to the container
+var conString = builder.Configuration.GetConnectionString("Connection");
+builder.Services.AddDbContext<AppContext>(option => option.UseSqlServer(conString));
+// Add services to the container.
+
+builder.Services.AddControllers();
+// Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
+builder.Services.AddEndpointsApiExplorer();
+builder.Services.AddSwaggerGen();
+
+var app = builder.Build();
+
+// Configure the HTTP request pipeline.
+if (app.Environment.IsDevelopment())
+{
+    app.UseSwagger();
+    app.UseSwaggerUI();
+}
+
+app.UseHttpsRedirection();
+
+app.UseAuthorization();
+
+app.MapControllers();
+
+app.Run();
