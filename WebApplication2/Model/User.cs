03b5f7221726bf2e177cae886fa3904baeba0920
veels3122 <mmelgarejo@ucundinamarca.edu.co>
2024-09-03 19:12:43 -0500

    Add project files.

---
+++ WebApplication2/Model/User.cs
@@ -0,0 +1,14 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace WebApplication2.Model
+{
+    public class User
+    {
+        [Key]
+        public int Id { get; set; }
+        public required string Name { get; set; }
+        public required string Email { get; set; }
+        public required string Password { get; set; }
+        public required string Username { get; set;}
+    }
+}
