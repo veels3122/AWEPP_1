d07096d97847375fb29ef24308c62987fc96b8e7
Albeirorp <nalbeirorincon@ucundinamarca.edu.co>
2024-09-25 09:20:20 -0500

    codigo de ana

---
+++ AWEPP/AWEPP/Model/TypeAccesUser.cs
@@ -0,0 +1,13 @@
+﻿using AWEPP.Modelo;
+using System.ComponentModel.DataAnnotations;
+
+namespace AWEPP.Model
+{
+    public class TypeAccesUser
+    {
+        
+    [Key]
+        public int Id { get; set; }
+        public virtual required TypeAcces TypeAcces { get; set; }
+    }
+}
---
+++ AWEPP/AWEPP/Repositories/Accestype_Repository.cs
@@ -0,0 +1,11 @@
+﻿namespace AWEPP.Repositories
+{
+    public class Accestype_Repository
+    {
+        Task<IEnumerable<Accestype_Repository>> GetAllAccestype_RepositoryAsync();
+        Task<Accestype_Repository> GetAccestype_RepositoryByIdAsync(int Id);
+        Task CreateAccestype_RepositoryAsync(Accestype_Repository accestype_repository);
+        Task CreateAccestype_RepositoryAsync(Accestype_Repository accestype_repository);
+        Task SoftDeleteAccestype_RepositoryAsync(int Id);
+    }
+}
