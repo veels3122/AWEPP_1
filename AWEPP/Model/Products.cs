750d9e537e033f07f61dfe1738a9d6a164e1f2db
veels3122 <mmelgarejo@ucundinamarca.edu.co>
2024-09-27 14:32:32 -0500

    arreglo del bug

---
+++ AWEPP/Model/Products.cs
@@ -0,0 +1,28 @@
+﻿using AWEPP.Modelo;
+using System.ComponentModel.DataAnnotations;
+
+namespace AWEPP.Model
+{
+    public class Products
+    {
+        [Key]
+        public int Id { get; set; }
+        public virtual required TypeAccounts TypeAccounts { get; set; }
+        public virtual required TypeProducts TypeProducts { get; set; }
+        public virtual required Bank Bank { get; set; }
+        public required string Account { get; set; }
+        public required string NumberAcount { get; set; }
+        public required string TotalBalance { get; set; }
+        public required string PartialBalance { get; set; }
+        public required string Debt { get; set; }
+        public required string DatePayment { get; set; }
+        public required string Description { get; set; }
+        public virtual required Customer Customer { get; set; }
+
+    }
+}
+
+
+
+
+ 
---
+++ AWEPP/Model/TypeIdenty.cs
@@ -0,0 +1,11 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace AWEPP.Model
+{
+    public class TypeIdenty
+    {
+        [Key]
+        public int Id { get; set; }
+        public required string TipoIdenty { get; set; }
+    }
+}
