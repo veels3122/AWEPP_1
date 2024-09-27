6dd3e7febc1078d81bd018d586f7f41b1ad5402c
veels3122 <mmelgarejo@ucundinamarca.edu.co>
2024-09-27 11:50:31 -0500

    cambio de i a i mayuscula

--- AWEPP/AWEPP/Migrations/20240925163401_initial.cs
+++ AWEPP/AWEPP/Migrations/20240925163401_initial.cs
@@ -5,7 +5,7 @@
 namespace AWEPP.Migrations
 {
   
-    public partial class initial : Migration
+    public partial class Initial : Migration
     {
      
         protected override void Up(MigrationBuilder migrationBuilder)
