6393a402c70ca7a859f6ae4a7398d79fb982e0f9
veels3122 <mmelgarejo@ucundinamarca.edu.co>
2024-09-28 10:22:51 -0500

    modificacion del type aces y creacion del repositorio typeaccesuser

---
+++ AWEPP_/AWEPP/Repositories/ITypeAccesUserRepository.cs
@@ -0,0 +1,44 @@
+﻿using AWEPP.Context;
+using AWEPP.Model;
+using AWEPP.Modelo;
+using Microsoft.EntityFrameworkCore;
+using System.Threading.Tasks;
+
+namespace AWEPP.Repositories
+{
+    public interface ITypeAccesUserRepository
+    {
+        Task<IEnumerable<TypeAccesUser>> GetAllTypeAccesUsersAsync();
+        Task<TypeAccesUser> GetTypeAccesUserByIdAsync(int id);
+        Task<TypeAccesUser> CreateTypeAccesUserAsync(TypeAccesUser typeAccesUser);
+        Task<TypeAccesUser> UpdateTypeAccesUserAsync(TypeAccesUser typeAccesUser);
+        Task SoftDeleteTypeAccesUserAsync(int id);
+    }
+    public class TypeAccesUserRepository : ITypeAccesUserRepository
+    {
+        public Task<TypeAccesUser> CreateTypeAccesUserAsync(TypeAccesUser typeAccesUser)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task<IEnumerable<TypeAccesUser>> GetAllTypeAccesUsersAsync()
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task<TypeAccesUser> GetTypeAccesUserByIdAsync(int id)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task SoftDeleteTypeAccesUserAsync(int id)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task<TypeAccesUser> UpdateTypeAccesUserAsync(TypeAccesUser typeAccesUser)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
