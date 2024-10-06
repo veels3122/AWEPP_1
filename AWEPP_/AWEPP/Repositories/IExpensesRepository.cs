923fb1e53065489b2c693a76418efdf0ba99e0ce
ane0126 <92052436+ane0126@users.noreply.github.com>
2024-10-05 20:20:50 -0500

    Repos

--- AWEPP_/AWEPP/Repositories/IExpensesRepository.cs
+++ AWEPP_/AWEPP/Repositories/IExpensesRepository.cs
@@ -1,62 +1,15 @@
-﻿using AWEPP.Context;
-using AWEPP.Model;
-using Microsoft.EntityFrameworkCore;
-using System.Threading.Tasks;
+﻿using AWEPP.Model;
 
 namespace AWEPP.Repositories
 {
     public interface IExpensesRepository
+
     {
         Task<IEnumerable<Expenses>> GetAllExpensesAsync();
-        Task<Expenses> GetExpenseByIdAsync(int id);
-        Task<Expenses> CreateExpenseAsync(Expenses expense);
-        Task<Expenses> UpdateExpenseAsync(Expenses expense);
-        Task SoftDeleteExpenseAsync(int id);
+        Task<Expenses> GetExpensesByIdAsync(int Id);
+        Task CreateExpensesAsync(Expenses expenses);
+        Task UpdateExpensesAsync(Expenses expenses);
+        Task SoftDeleteExpensesAsync(int Id);
     }
-    public class ExpensesRepository : IExpensesRepository
-    {
-        private readonly Aweppcontext _context;
-
-        public ExpensesRepository(Aweppcontext context)
-        {
-            _context = context;
-        }
-
-        public async Task<IEnumerable<Expenses>> GetAllExpensesAsync()
-        {
-            return await _context.Expenses.ToListAsync();
-        }
-
-        public async Task<Expenses> GetExpenseByIdAsync(int id)
-        {
-#pragma warning disable CS8603 //possible null return
-            return await _context.Expenses.FirstOrDefaultAsync(e => e.Id == id);
-        }
-#pragma warning disable CS8603 //possible null return
-        public async Task<Expenses> CreateExpenseAsync(Expenses expense)
-        {
-            _context.Expenses.Add(expense);
-            await _context.SaveChangesAsync();
-            return expense;
-        }
-
-        public async Task<Expenses> UpdateExpenseAsync(Expenses expense)
-        {
-            _context.Entry(expense).State = EntityState.Modified;
-            await _context.SaveChangesAsync();
-            return expense;
-        }
-
-        public async Task
-     SoftDeleteExpenseAsync(int id)
-        {
-            var expense = await _context.Expenses.FindAsync(id);
-            if (expense != null)
-            {
-                // Implement soft delete logic here (e.g., set a flag)
-                // expense.IsDeleted = true;
-                // await _context.SaveChangesAsync();
-            }
-        }
-    }
+    
 }
