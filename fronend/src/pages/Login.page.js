dfdc61562d49f97320b75c7b7b382748182f668b
Andres Rodriguez <ayesidrodriguez@ucundinamarca.edu.co>
2024-10-17 21:10:10 -0500

    login

---
+++ fronend/src/pages/Login.page.js
@@ -0,0 +1,12 @@
+import React from "react";
+import LoginForm from "../components/Login.form";
+
+const LoginPage = () => {
+  return (
+      <div className="login-page">
+          <LoginForm />
+      </div>
+  );
+};
+
+export default LoginPage;
