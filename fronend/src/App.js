81bce3b71384abda72872cb5081c694c84fed2cb
Andres Rodriguez <ayesidrodriguez@ucundinamarca.edu.co>
2024-10-23 00:13:39 -0500

    inicio

--- fronend/src/App.js
+++ fronend/src/App.js
@@ -1,14 +1,19 @@
 import "./App.css";
-import LoginPage from "./pages/Login.page";
 import "primereact/resources/themes/fluent-light/theme.css";
 import "primereact/resources/primereact.min.css";
 import "primeicons/primeicons.css";
+import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
+import LoginPage from './pages/LoginPage'; 
+import Inicio from './pages/Inicio';   
 
 function App() {
   return (
-    <>
-      <LoginPage />
-    </>
+        <Router>
+            <Routes>
+                <Route path="/login" element={<LoginPage />} />
+                <Route path="/inicio" element={<Inicio />} />
+            </Routes>
+        </Router>
   );
 }
 
